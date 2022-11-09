72b0c7e509a531f48630594e03dfb4f5242da2e3
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-11-09 22:59:30 +0600

    javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+// console.log("i\"m jihan");
+// console.log('i\'m jinnurain');
+let name=prompt("Name");
+let age=prompt("Age");
+let skill=prompt("skill");
+
+// console.log("my name is "+name+" & i am "+age+" years old"+" & i have skill on "+skill);
+console.log(`my name is ${name} & i am ${age} years old & i have skill on ${skill}`);
